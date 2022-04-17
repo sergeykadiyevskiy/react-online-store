43af1553843d037651708540bd77d4d87315ba26
Sergey Kadiyevskiy <51814805+Kalashnik0v1947@users.noreply.github.com>
2022-04-17 15:07:51 -0400

    all routes added

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,11 @@ const App = () => {
     <Router>
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path='/products' element={<ProductList/>}/>
+        <Route path='/products/:category' element={<ProductList/>}/>
+        <Route path='/product/:id' element={<Product/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
                
       </Routes>
     </Router>
